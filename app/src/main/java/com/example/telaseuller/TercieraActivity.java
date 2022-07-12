17f4c1e4e51fb16ff75ed6eea1432880db18bda9
eulleramos <95365083+eulleramos@users.noreply.github.com>
2022-07-12 11:58:10 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/telaseuller/TercieraActivity.java
@@ -0,0 +1,14 @@
+package com.example.telaseuller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TercieraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terciera);
+    }
+}
